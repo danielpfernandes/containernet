a19d19923ab8f2b535a7b02728f0c92035c898d0
Ramon Fontes <ramonreisfontes@gmail.com>
2024-08-22 07:43:00 -0300

    some code adjustments

--- examples/containernet_p4.py
+++ examples/containernet_p4.py
@@ -18,20 +18,17 @@ def topology():
     h2 = net.addHost('h2', mac="00:00:00:00:00:02")
 
     path = os.path.dirname(os.path.abspath(__file__))
-    json_file = '/root/s1-runtime.json'
+    json_file = '/root/s1-runtime.json' # container directory
     config = path + '/p4_files/commands_s1.txt'
     args = {'json': json_file, 'switch_config': config}
 
     info('*** Adding P4 Switch\n')
     # IPBASE: subnet from eth0 interface,
     s1 = net.addSwitch('s1', cls=DockerP4Switch,
-                        volumes=[path + "/p4_files:/root"],
-                        dimage="ramonfontes/bmv2", cpu_shares=20,
+                       volumes=[path + "/p4_files:/root"],
+                       dimage="ramonfontes/bmv2", cpu_shares=20,
                        netcfg=True, thriftport=50001,
-                        IPBASE="172.17.0.0/16", **args)
-
-    info('*** Configuring WiFi nodes\n')
-    net.configureWifiNodes()
+                       IPBASE="172.17.0.0/16", **args)
 
     net.addLink(h1, s1)
     net.addLink(h2, s1)
